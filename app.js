151cf6678f228e2905ae58148160eb2a747c459b
JMGarofalo <83677317+JMGarofalo@users.noreply.github.com>
2022-11-09 15:50:49 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,45 @@
 
+
 /* VALIDACION FORMULARIO */
 
 function validar(){
-    var nombreValido = validarNombre();
-    var emailValido = validarEmail();
-    var mensajeValido = validarMensaje();
+    var nombreValido = validarNombre()
+    var emailValido = validarEmail()
+    var mensajeValido = validarMensaje()
     return (nombreValido && emailValido && mensajeValido);
 }
 
 function validarNombre(){
+    var esValido = true;
     var x=document.myform.nombre.value;
     x = x.replace(/\s+/g, "");
     if (x=="") {
-        alert("Ingresar un nombre válido");
-        return false;
+        alert("Ingresar un nombre");
+        esValido = false;
     }
+    return esValido;
 }
-function validarEmail()  {  
+
+function validarEmail()  {
+    var esValido = true; 
     var x=document.myform.email.value;  
     var atposition=x.indexOf("@");  
     var dotposition=x.lastIndexOf(".");  
     if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
         alert("Ingresar un email válido");  
-        return false;  
-    }  
+        esValido = false;  
+    }
+    return esValido;  
 }  
 
 function validarMensaje(){
+    var esValido = true;
     var x=document.myform.mensaje.value;
     x = x.replace(/\s+/g, "");
     if (x=="") {
         alert("Ingresar un mensaje");
-        return false;
+        esValido = false;
     }
+    return esValido;
 }
-
 
