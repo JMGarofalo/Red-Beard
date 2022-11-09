9e6edd9f04c1e0ee13fd78de0317b3fca2b3b76d
JMGarofalo <83677317+JMGarofalo@users.noreply.github.com>
2022-11-09 19:53:32 -0300

    Update app.js

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ function validar(){
 
 function validarNombre(){
     let esValido = true;
-    let x = document.myform.nombre.value;
-    x = x.replace(/\s+/g, "");
-    if (x=="") {
+    let nombre = document.myform.nombre.value;
+    nombre = nombre.trim();
+    if (nombre=="") {
         alert("Ingresar un nombre");
         esValido = false;
     }
@@ -22,10 +22,10 @@ function validarNombre(){
 
 function validarEmail()  {
     let esValido = true; 
-    let x = document.myform.email.value;  
-    let atposition = x.indexOf("@");  
-    let dotposition = x.lastIndexOf(".");  
-    if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
+    let email = document.myform.email.value;  
+    let atposition = email.indexOf("@");  
+    let dotposition = email.lastIndexOf(".");  
+    if (atposition<1 || dotposition<atposition+2 || dotposition+2>=email.length){  
         alert("Ingresar un email válido");  
         esValido = false;  
     }
@@ -34,9 +34,10 @@ function validarEmail()  {
 
 function validarMensaje(){
     let esValido = true;
-    let x = document.myform.mensaje.value;
-    x = x.replace(/\s+/g, "");
-    if (x=="") {
+    let mensaje = document.myform.mensaje.value;
+    //x = x.replace(/\s+/g, "");
+    mensaje = mensaje.trim();
+    if (mensaje=="") {
         alert("Ingresar un mensaje");
         esValido = false;
     }
