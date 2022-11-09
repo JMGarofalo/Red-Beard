7060cb2ebe57b6aca006207e4ed51c1e0b169bcd
JMGarofalo <83677317+JMGarofalo@users.noreply.github.com>
2022-11-09 20:28:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 
 /* VALIDACION FORMULARIO */
 
-function validar(){
+function validar() {
     let nombreValido = validarNombre()
     let emailValido = validarEmail()
     let mensajeValido = validarMensaje()
     return (nombreValido && emailValido && mensajeValido);
 }
 
-function validarNombre(){
+
+function validarNombre() {
     let esValido = true;
-    let nombre = document.myform.nombre.value;
-    nombre = nombre.trim();
+    let nombre = document.myform.nombre.value.trim();
     if (nombre=="") {
         alert("Ingresar un nombre");
         esValido = false;
@@ -19,26 +19,31 @@ function validarNombre(){
     return esValido;
 }
 
-function validarEmail()  {
+
+function validarEmail() {
     let esValido = true; 
-    let email = document.myform.email.value;  
+    let email = document.myform.email.value.trim();  
     let atposition = email.indexOf("@");  
-    let dotposition = email.lastIndexOf(".");  
-    if (atposition<1 || dotposition<atposition+2 || dotposition+2>=email.length){  
-        alert("Ingresar un email válido");  
-        esValido = false;  
+    let dotposition = email.lastIndexOf(".");
+    if (email=="") {
+        alert("Ingresar un email");  
+        esValido = false;
+    } else if (atposition<1 || dotposition<atposition+2 || dotposition+2>=email.length || email.includes(" ")) {
+            alert("Ingresar un email válido");  
+            esValido = false;  
     }
     return esValido;  
 }  
 
-function validarMensaje(){
+
+function validarMensaje() {
     let esValido = true;
-    let mensaje = document.myform.mensaje.value;
-    mensaje = mensaje.trim();
+    let mensaje = document.myform.mensaje.value.trim();
     if (mensaje=="") {
         alert("Ingresar un mensaje");
         esValido = false;
     }
     return esValido;
 }
+
 
